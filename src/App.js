8762472b961c843d3652611f959d97bfac5058a4
Florian <flo@mzechmeister.de>
2020-05-08 23:15:17 +0200

    fetch from api on mount

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      hello you
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			data = null,
+		};
+	}
+
+	componentDidMount() {
+		fetch('https://www.govdata.de/ckan/api/3/action/organization_list?all_fields=true')
+			.then(response => response.json())
+			.then(data => this.setState({ data }));
+	}
+
+	render () {
+		return (
+	    <div className="App">
+	      {this.state.data}
+	    </div>
+	  );
+	}
+  
 }
 
 export default App;
